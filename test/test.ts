ca09685f24f3562cc54d5500b745307fc2d24d05
Benjie Gillam <benjie@jemjie.com>
2019-09-03 17:48:39 +0100

    Tests defined by input.txt file

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,8 @@
 import test from 'ava'
 import { sync } from 'glob'
 import { readFile, writeFile } from 'mz/fs'
-import { basename, resolve } from 'path'
+import { basename, dirname, resolve } from 'path'
 import { compile } from '../src'
-
-let paths = ['rules', 'unit']
 
 // Kind of hacky thing to figure out if -u or --update-snapshots was passed to AVA.
 // We manually write expected output in this test.
@@ -15,31 +13,27 @@ const argvOriginal = argv.original || []
 const update =
   argvOriginal.includes('-u') || argvOriginal.includes('--update-snapshots')
 
-paths.forEach(path => {
-  // TODO: Why does glob catch tslint.json even with the trailing slash?
-  const folders = sync(resolve(__dirname, `../../test/${path}/*/`))
-    .filter(_ => !_.endsWith('.json'))
-    .filter(_ => !basename(_).startsWith('_'))
+const inputPaths = sync(resolve(__dirname, `../../test/**/input.txt`))
+const tests = inputPaths.map(inputPath => ({
+  inputPath,
+  outputPath: dirname(inputPath) + '/output.txt',
+  name: basename(dirname(inputPath))
+}))
 
-  const tests = folders.map(folder => ({ folder, name: basename(folder) }))
-
-  tests.forEach(({ name, folder }) =>
-    test(name, async t => {
-      try {
-        const inputPath = resolve(folder, 'input.txt')
-        const outputPath = resolve(folder, 'output.txt')
-        const input = await readFile(inputPath, 'utf-8')
-        const output = await compile(input, inputPath)
-        if (update) {
-          await writeFile(outputPath, output)
-          t.pass()
-        } else {
-          const expectedOutput = await readFile(outputPath, 'utf-8')
-          t.is(output, expectedOutput)
-        }
-      } catch (e) {
-        console.log('error', e)
+tests.forEach(({ name, inputPath, outputPath }) =>
+  test(name, async t => {
+    try {
+      const input = await readFile(inputPath, 'utf-8')
+      const output = await compile(input, inputPath)
+      if (update) {
+        await writeFile(outputPath, output)
+        t.pass()
+      } else {
+        const expectedOutput = await readFile(outputPath, 'utf-8')
+        t.is(output, expectedOutput)
       }
-    })
-  )
-})
+    } catch (e) {
+      console.log('error', e)
+    }
+  })
+)

8c9b48d845ed1c726d13f577fef16a7506604d57
Benjie Gillam <benjie@jemjie.com>
2019-11-25 12:32:56 +0000

    Backfill missing comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { parse } from '@babel/parser'
 import generate from '@babel/generator'
 import traverse, { Node, Visitor, VisitNode } from '@babel/traverse'
-import { File } from '@babel/types'
+import { File, CommentBlock, CommentLine } from '@babel/types'
 import { dropWhile, pullAt } from 'lodash'
 import { EOL } from 'os'
 import { relative } from 'path'
@@ -14,7 +14,7 @@ export async function compile(code: string, filename: string) {
     plugins: ['classProperties', 'flow', 'objectRestSpread'],
     sourceType: 'module'
   })
-  let [warnings, ast] = await convert(parsed)
+  let [warnings, ast] = await convert(parsed, code)
 
   warnings.forEach(([message, issueURL, line, column]) => {
     console.log(
@@ -34,14 +34,95 @@ export async function compile(code: string, filename: string) {
   )
 }
 
+function backfillMissingComments<T extends Node>(ast: T, code: string): void {
+  // Add missing comments
+  const comments: (CommentBlock | CommentLine)[] = (ast as any).comments
+  const seenComments: (CommentBlock | CommentLine)[] = []
+
+  const checkSeen = (c: CommentBlock | CommentLine) => {
+    if (comments.includes(c) && !seenComments.includes(c)) {
+      seenComments.push(c)
+    }
+  }
+
+  traverse(ast, {
+    enter(path) {
+      const node = path.node
+      if (node.leadingComments) {
+        node.leadingComments.forEach(checkSeen)
+      }
+      if (node.trailingComments) {
+        node.trailingComments.forEach(checkSeen)
+      }
+      if (node.innerComments) {
+        node.innerComments.forEach(checkSeen)
+      }
+    }
+  })
+
+  const unseenComments: (CommentBlock | CommentLine)[] = []
+  comments.forEach(c => {
+    if (!seenComments.includes(c)) {
+      unseenComments.push(c)
+    }
+  })
+
+  if (unseenComments.length > 0) {
+    console.log(
+      'The following comments are not represented in leadingComments, trailingComments or innerComments of any node in the AST:'
+    )
+    console.log(unseenComments)
+    console.log('Attempting to add them back into the AST')
+    traverse(ast, {
+      enter(path) {
+        const node = path.node
+        for (const comment of unseenComments) {
+          if (
+            node.end &&
+            node.end < comment.start &&
+            node.end > comment.start - 3
+          ) {
+            const inBetween = code.substring(node.end, comment.start)
+            if (inBetween.match(/^(\s*[,;])?\s*$/)) {
+              if (!node.trailingComments) {
+                node.trailingComments = []
+              }
+              ;(node.trailingComments as any).push(comment)
+              unseenComments.splice(unseenComments.indexOf(comment), 1)
+            }
+          }
+        }
+      }
+    })
+    if (unseenComments.length > 0) {
+      console.error(
+        "The following comments couldn't be added back into the AST"
+      )
+      console.error(unseenComments)
+      throw new Error('Failed to move all comments into the AST; aborting')
+    }
+  }
+}
+
 /**
  * @internal
  */
-export async function convert(ast: File): Promise<[Warning[], File]>
+export async function convert(
+  ast: File,
+  code: string
+): Promise<[Warning[], File]>
 export async function convert<T extends Node>(
-  ast: T
+  ast: T,
+  code: string
+): Promise<[Warning[], Node]>
+export async function convert<T extends Node>(
+  ast: T,
+  code: string
 ): Promise<[Warning[], Node]> {
   let warnings: Warning[] = []
+
+  backfillMissingComments(ast, code)
+
   const outAst = toTs(ast, warnings)
   const visit: VisitNode<any, any> = path => {
     path.replaceWith(toTs(path.node, warnings))
